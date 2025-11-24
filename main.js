026bd17e134b380114629549d6a747658ce7966d
Ferran <ferran.garcia@institutvidreres.cat>
2025-11-24 11:15:40 +0100

    Exercici 3

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // EX1
-let a = Array.from({ length: 5 }, () => Math.floor(Math.random() * 100) + 1);
+let a = Array.from({ length: 5 }, () => Math.floor(Math.random() * 10) + 1);
 console.log("Array original:", a);
 a.sort((a, b) => a - b);
 console.log("Array ordenat:", a);
@@ -10,3 +10,12 @@ console.log("L'ultim número: ", a[a.length - 1])
 // EX2
 let b = [1, 2, 3, ...a];
 console.log("Nou array amb propagació:", b);
+
+// EX3
+let fusionat = [];
+for (let valor of [...a, ...b]) {
+    if (!fusionat.includes(valor)) {
+        fusionat.push(valor);
+    }
+}
+console.log("Array fusionat sense duplicats:", fusionat);
