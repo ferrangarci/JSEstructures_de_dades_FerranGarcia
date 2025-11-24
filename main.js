1118e69c241e5824a38e314a9aa9f3ee546821ea
Ferran <ferran.garcia@institutvidreres.cat>
2025-11-24 10:54:15 +0100

    Exercici 1

---
+++ main.js
@@ -0,0 +1,7 @@
+// EX1
+let a = new Array('5', '2', '5', '8', '6');
+console.log("Array original:", a);
+a.sort((a, b) => a - b);
+console.log("Array ordenat:", a);
+console.log('El primer número: ', a[0])
+console.log("L'ultim número: ", a[a.length - 1])
